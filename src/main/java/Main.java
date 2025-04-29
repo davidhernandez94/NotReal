040c3f75d6355cbfa4d9f3db4cb3b15dc1276ff7
David Hernandez <6309110@edu.vaniercollege.qc.ca>
2025-04-29 15:35:27 -0400

    write otherSort, insertionSort, and let bubbleSort choose between ascending and descending

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,24 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {5,2,6,3,8,1,5,2};
-        selectionSort(nums);
+        bubbleSort(nums, false);
         System.out.println(Arrays.toString(nums));
+    }
+
+    public static void bubbleSort(int[] nums) {
+        bubbleSort(nums, true);
+    }
+
+    public static void bubbleSort(int[] nums, boolean ascending) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1] ^ !ascending) { // exclusive or operator
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
     }
 
     public static void selectionSort(int[] nums) {
@@ -21,7 +37,19 @@ public class Main {
         }
     }
 
-    public static void bubbleSort(int[] nums) {
+    public static void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+    }
+
+    public static void otherSort(int[] nums) {
         int idx = 0;
         int temp;
         while (idx <= nums.length - 2) {
