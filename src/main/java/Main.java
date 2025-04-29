6d64f93b328efc8aac8908d334a0635c15c8efec
David Hernandez <6309110@edu.vaniercollege.qc.ca>
2025-04-29 15:03:55 -0400

    write bubbleSort and selectionSort in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,42 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {5,2,6,3,8,1,5,2};
+        selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[min];
+            nums[min] = temp;
+        }
+    }
+
+    public static void bubbleSort(int[] nums) {
+        int idx = 0;
+        int temp;
+        while (idx <= nums.length - 2) {
+            if (nums[idx] <= nums[idx + 1]) {
+                idx++;
+            } else {
+                temp = nums[idx];
+                nums[idx] = nums[idx + 1];
+                nums[idx + 1] = temp;
+                if (idx == 0) {
+                    idx++;
+                    continue;
+                }
+                idx--;
+            }
+        }
+    }
 }
